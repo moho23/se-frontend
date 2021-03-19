8458d14a94c2416c48a8176559c03b533326f5be
Mohammadhossein Hosseinpour <78mohammadhossein@gmail.com>
2021-03-19 22:30:34 +0330

    Base Project

--- src/MainProject/mainproject.index.js
+++ src/MainProject/mainproject.index.js
@@ -1,18 +1,12 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import "./mainproject.style.scss"
-import {toast} from "react-toastify";
 
 
 const MainProject = () => {
     return (
         <Router>
             <Switch>
-                <Route path="*">
-                    <div  onClick={()=>toast.success("salam")} className="main-project-page">
-                        Hello World!
-                    </div>
-                </Route>
             </Switch>
         </Router>
     )
