99105975789e270d957c4e6b14eaf4e9c1493ad2
Mohammadhossein Hosseinpour <78mohammadhossein@gmail.com>
2021-05-24 17:14:33 +0430

    Fix: style

--- src/MainProject/myLandscapes/myLandscapes.index.js
+++ src/MainProject/myLandscapes/myLandscapes.index.js
@@ -6,23 +6,47 @@ import {get, responseValidator} from "../../scripts/api";
 import {toast} from "react-toastify";
 import {Link} from "react-router-dom";
 import noData from "../../assets/images/undraw_not_found_60pq.svg"
+import {Button, Modal, Tooltip} from "antd";
 
 
 const MyLandscapes = () => {
 
     const [landscapes, setLandscapes] = useState(null);
-    const [check, setCheck] = useState(true)
 
     useEffect(() => {
         get(APIPath.map.myLandscapes).then((data) => {
             if (responseValidator(data.status) && data.data) {
-                console.log(data.data)
                 setLandscapes(data.data)
             } else {
                 toast.error("سیستم با خطا مواجه شد، مجددا تلاش کنید");
             }
         });
     }, [])
+
+    const [visible, setVisible] = React.useState(false);
+
+    const showModal = () => {
+        setVisible(true);
+    };
+
+    const handleOk = () => {
+        setVisible(false);
+    };
+
+    const handleCancel = () => {
+        setVisible(false);
+    };
+
+    function isPersianOrEnglish(str) {
+        const alphabet = "1234567890abcdefghijklmnopqrstuvwxyz";
+        const temp = str?.toString()
+        for (let i = 0; i < alphabet.length; i++) {
+            if (temp?.split('')[0] === alphabet[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     return (
         <div className='my-landscape-page'>
@@ -35,13 +59,52 @@ const MyLandscapes = () => {
                                  src={item.image[0] ? item.image[0] : cover}/>
                         </div>
                         <div className='content'>
-                            <p className="name">{item.name}</p>
-                            <p className="address">{item.address}</p>
-                            <p className={check ? "description" : "description-no"}>{item.description}</p>
+                            <p className={`${isPersianOrEnglish(item.name) === false ? 'name-address' : 'name-address is-english'}`}>{item.name && item.name.length > 12 ? item.name.substring(0, 13) + '...' : item.name}</p>
+                            <Tooltip placement="right" title={item.address}>
+                                <p className={`${isPersianOrEnglish(item.address) === false ? 'name-address' : 'name-address is-english'}`}>{item.address && item.address.length > 20 ? item.address.substring(0, 20) + '...' : item.address}</p>
+                            </Tooltip>
+                            <Tooltip placement="right" title={item.description}>
+                                <p className={`${isPersianOrEnglish(item.description) === false ? 'description' : 'description is-english'}`}>{item.description && item.description.length > 60 ? item.description.substring(0, 60) + '...' : item.description}</p>
+                            </Tooltip>
+                            <span/>
+                            <p className="delete" onClick={showModal}>حذف</p>
                         </div>
                     </div>
                 ))
             }
+            <div className="my-grid"/>
+            <div className="my-grid"/>
+            <Modal
+                visible={visible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                className="modal"
+                footer={<div style={{display: "flex", width: "100%"}}>
+                    <Button style={{
+                        outline: "none",
+                        border: "none",
+                        color: "white",
+                        backgroundColor: "green",
+                        borderRadius: "5px"
+                    }}>تایید</Button>
+                    <Button style={{
+                        outline: "none",
+                        border: "none",
+                        backgroundColor: "orange",
+                        borderRadius: "5px"
+                    }}>لغو</Button>
+                </div>}
+            >
+                <p style={{
+                    marginTop: "25px",
+                    marginBottom: "-10px",
+                    display: "flex",
+                    width: "100%",
+                    justifyContent: "flex-end",
+                    fontWeight: 500
+                }} className="modal-text">آیا
+                    از حذف این مکان مطمئن هستید؟</p>
+            </Modal>
             {
                 landscapes &&
                 landscapes.length === 0 && <div className="no-data">
